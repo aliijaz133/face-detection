603d309edf1b09b3faaf4b641d28b5ae78440207
Ali Ijaz <67383788+aliijaz133@users.noreply.github.com>
2024-01-28 20:21:46 +0500

    Updated

--- frontend/main_file.js
+++ frontend/main_file.js
@@ -1,5 +1,5 @@
 function faceDetection() {
-  console.log('faceDetection');
+  console.log("faceDetection");
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve();
@@ -7,24 +7,23 @@ function faceDetection() {
   });
 
   //code for face detection
-   const canvas = document.getElementById('canvas');
-   const ctx = canvas.getContext('2d');
-   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-   const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-   const faces = faceapi.detectAllFaces(imgData);
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
-   faces.forEach((face) => {
-     ctx.beginPath();
-     ctx.arc(face.x, face.y, face.width / 2, 0, 2 * Math.PI);
-     ctx.fillStyle = '#FF0000';
-     ctx.fill();
-     ctx.closePath();
-   });
-
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const faces = faceapi.detectAllFaces(imgData);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  faces.forEach((face) => {
+    ctx.beginPath();
+    ctx.arc(face.x, face.y, face.width / 2, 0, 2 * Math.PI);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    ctx.closePath();
+  });
 }
 
 function clearCanvas() {
-  console.log('clearCanvas');
+  console.log("clearCanvas");
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve();
@@ -32,15 +31,13 @@ function clearCanvas() {
   });
 
   //code for Canvas clear
-   const canvas = document.getElementById('canvas');
-   const ctx = canvas.getContext('2d');
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-
-  }
-
-  function saveCanvas() {
-  console.log('saveCanvas');
+function saveCanvas() {
+  console.log("saveCanvas");
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve();
@@ -48,50 +45,59 @@ function clearCanvas() {
   });
 
   //code for Canvas save
-   const canvas = document.getElementById('canvas');
-   canvas.toBlob((blob) => {
-     const url = URL.createObjectURL(blob);
-     const a = document.createElement('a');
-     a.href = url;
-     a.download = 'canvas.png';
-     a.click();
-   });
-  }
+  const canvas = document.getElementById("canvas");
+  canvas.toBlob((blob) => {
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "canvas.png";
+    a.click();
+  });
+}
 
-  function loadCanvas() {
-  console.log('loadCanvas');
+function loadCanvas() {
+  console.log("loadCanvas");
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve();
     }, 1000);
   });
   //code for Canvas load
-   const canvas = document.getElementById('canvas');
-   canvas.toBlob((blob) => {
-     const url = URL.createObjectURL(blob);
-     const a = document.createElement('a');
-     a.href = url;
-     a.download = 'canvas.png';
-     a.click();
-   });
-
-  }
+  const canvas = document.getElementById("canvas");
+  canvas.toBlob((blob) => {
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "canvas.png";
+    a.click();
+  });
+}
 
 function loadImage() {
-  console.log('loadImage');
+  console.log("loadImage");
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve();
     }, 1000);
   });
   //code for Image load
-   const canvas = document.getElementById('canvas');
-   canvas.toBlob((blob) => {
-     const url = URL.createObjectURL(blob);
-     const a = document.createElement('a');
-     a.href = url;
-     a.download = 'canvas.png';
-     a.click();
-   });
+  const canvas = document.getElementById("canvas");
+  canvas.toBlob((blob) => {
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "canvas.png";
+    a.click();
+  });
+}
 
-  }
+function setLocalTime() {
+  let getTime = document.getElementById('timeNow');
+
+  setInterval(() => {
+      getTime.innerText = new Date().toLocaleTimeString();
+  }, 1000);
+}
+
+// Call the function to start updating the time
+setLocalTime();
